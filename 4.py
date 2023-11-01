fb8f78b98270c7210beffcb1b3d655e684a13a33
aaaaaxl07 <136476521+aaaaaxl07@users.noreply.github.com>
2023-11-02 02:10:45 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -46,45 +46,36 @@ for xd in range(5000):
     V=str(random.randrange(11,99))
     uas=f'{aa} {bv}; {c}{d}{e}{f} Build/{d}{f}{V}{f}; wv) {g}{h}.{i}.{j}.{k} {l}'
     ugen.append(uas)
-logo4 = """
-\x1b[1;91m
-\x1b[1;92m
-\x1b[1;96m
-\x1b[1;92m         {|} {|}  {|} {|}{|}{|} {|}{|}{|}
-\x1b[1;97m         {|} {|}  {|}    {|}       {|}
-\x1b[1;93m         {|} {|}  {|}    {|}       {|}
-\x1b[1;96m         {|} {|}  {|}    {|}       {|}
-\x1b[1;94m     {|}{|}   {|}{|}     {|}       {|}
-\x1b[1;93m
-\x1b[1;92m         Jutt Badshah Brand~
-\x1b[1;91m-----------------------------------------------
-\x1b[1;97m> Author :  prekarsha hutto
-\x1b[1;97m> Github : https://github.com/SHOOTER-MAKER
-\x1b[1;97m> Facebok: Jutt Badshah
-\x1b[1;97m> Version: any 3 min after airplane mod on/off 
-\x1b[0;97m-----------------------------------------------"""
-boy = ['Ali Khan', 'Rustam Khan', 'Faisal Khan', 'Afzal Khan', 'Haider Khan', 'Suleman Khan', 'Nadeem Khan', 'Nazeer Malik', 'Nazeer Jutt', 'Nazeer Rehmani', 'Safdar Malik', 'Intzar Khan', 'Saleem Malik', 'Abdullah Malik', 'Naseer Jutt', 'Muzammil Malik', 'Fiaz Ahmad', 'Asghar Ali', 'Shabeer Ahmad', 'Irfan Ali', 'Ahmad Gujjar', 'prekash khan', 'sadun khan', 'isuru kahan', 'jag ruwn', 'appo lhan', 'penda hutto', 'pre isur', 'janaki yako', 'oskage ahasa', 'kumuthu idu', 'achintha kau', 'palat yak', 'lomema mage', 'ranilge atta', 'bhandari yako', 'hum yako', 'Sajida Malik', 'Ayesha Khan', 'Nabeela Malik', 'Kinza Fatima', 'Arooj Khan', 'Muskan Khan', 'Ayesha Malik', 'Safina Malik', 'Nida Ali', 'Rimsha Ali']
-girl = ['Sajida Malik', 'Ayesha Khan', 'Nabeela Malik', 'Kinza Fatima', 'Arooj Khan', 'Muskan Khan', 'Ayesha Malik', 'Safina Malik', 'Nida Ali', 'Rimsha Ali']
+logo4 = """\33[1;32m
+
+                 █████╗ ██╗  ██╗██╗     
+                ██╔══██╗╚██╗██╔╝██║     
+                ███████║ ╚███╔╝ ██║     
+                ██╔══██║ ██╔██╗ ██║     
+                ██║  ██║██╔╝ ██╗███████╗
+                ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+\33[1;32m-------------------------------------------------------- 
+  Owner      :  Alexander Grayson
+  Facebook   :  AlexanderGraysonRecovery.IAmLimitless
+  Tool Type  :  RPW Facebook Cloning Tool (Paid)
+  Network    :  All Network
+  Version    :  3.5
+\33[1;32m--------------------------------------------------------"""
+boy = ['Ethan Parker', 'Liam Anderson', 'Mason Taylor', 'Aiden Mitchell', 'Lucas Bennett', 'Oliver Cooper', 'Carter Walker', 'Jackson Brooks', 'Noah Scott', 'Avery Foster', 'William Hayes', 'James Turner', 'Michael Bryant', 'Benjamin Carter', 'Daniel Russell', 'Alexander Parker', 'Matthew Foster', 'Henry Mitchell', 'Samuel Harrison', 'David Brooks', 'Joseph Anderson', 'John Davis', 'Nicholas Wright', 'Andrew Reed', 'Jonathan Stewart', 'Christopher Kelly', 'Gabriel Price', 'Caleb Butler', 'Nathaniel Hughes', 'Anthony Morgan', 'Nicholas Sullivan', 'Christopher Powell', 'Sebastian Price', 'Zachary Murphy', 'Matthew Bennett', 'Dominic Hill', 'William Ward', 'Isaac King', 'Landon Harrison', 'Eli Foster', 'Logan Turner', 'Luke Hayes', 'Owen Mitchell', 'Wyatt Cooper', 'Henry Reid', 'Elijah Anderson', 'William Turner', 'Samuel Bryant', 'Oliver Russell', 'Ethan Parker', 'Liam Anderson', 'Mason Taylor', 'Aiden Mitchell', 'Lucas Bennett', 'Oliver Cooper', 'Carter Walker', 'Jackson Brooks', 'Noah Scott', 'Avery Foster', 'William Hayes', 'James Turner', 'Michael Bryant', 'Benjamin Carter', 'Daniel Russell', 'Alexander Parker', 'Matthew Foster', 'Henry Mitchell', 'Samuel Harrison', 'David Brooks', 'Joseph Anderson', 'John Davis', 'Nicholas Wright', 'Andrew Reed', 'Jonathan Stewart', 'Christopher Kelly', 'Gabriel Price', 'Caleb Butler', 'Nathaniel Hughes', 'Anthony Morgan', 'Nicholas Sullivan', 'Christopher Powell', 'Sebastian Price', 'Zachary Murphy', 'Matthew Bennett', 'Dominic Hill', 'William Ward', 'Isaac King', 'Landon Harrison', 'Eli Foster', 'Logan Turner', 'Luke Hayes', 'Owen Mitchell', 'Wyatt Cooper', 'Henry Reid', 'Elijah Anderson', 'William Turner', 'Samuel Bryant', 'Oliver Russell',
+'Henry Porter', 'Maxwell Gray', 'Theodore Baker', 'Harrison Carter', 'Ezra Foster', 'Nolan Thompson', 'Lincoln Adams', 'Miles Evans', 'Nathaniel Hayes', 'Julian Sullivan', 'Oliver Foster', 'Benjamin Griffin', 'Alexander Fisher', 'Wyatt Richards', 'Oscar Turner', 'Hudson Wright', 'Sebastian Knight', 'Landon Parker', 'Caleb Brooks', 'Eli Stewart', 'Lucas Harrison', 'Mason Reynolds', 'Owen Powell', 'Daniel Morgan', 'James Bennett', 'Matthew Walker', 'Ethan Reed', 'Logan Turner', 'Aiden Murphy', 'Nathaniel Turner', 'William Griffin', 'Samuel Hayes', 'Elijah Foster', 'Christopher Bryant', 'Avery Price', 'David Mitchell', 'Gabriel Evans', 'Jackson Parker', 'Joseph Adams', 'Liam Bennett', 'Michael Richards']
+
+girl = ['Aria Grace', 'Luna Violet', 'Aurora Jade', 'Scarlett Evangeline', 'Mila Rose', 'Stella Celeste', 'Amara Seraphina', 'Layla Skye', 'Vivienne Elise', 'Eleanor Ruby', 'Hazel Quinn', 'Aveline Ivy', 'Isla Belle', 'Nora Hazel', 'Willow Joy', 'Athena Pearl', 'Lila Maeve', 'Sophia Harper', 'Olivia Celestia', 'Isabella Claire', 'Ella Primrose', 'Grace Seraphina', 'Chloe Aurora', 'Lily Juliet', 'Ariana Eloise', 'Amelia Hope', 'Emily Florence', 'Scarlett Belle', 'Victoria Sage', 'Madeline Faye', 'Cora Everly', 'Harper Luna', 'Evelyn Aurora', 'Aria Willow', 'Luna Grace', 'Aurora Mae', 'Stella Violet', 'Vivienne Rose', 'Mila Jade', 'Layla Quinn', 'Scarlett Elise', 'Hazel Ruby', 'Aveline Seraphina', 'Isla Skye', 'Nora Celeste', 'Willow Seraphina', 'Athena Ivy', 'Lila Belle', 'Sophia Pearl', 'Olivia Maeve', 'Aria Grace', 'Luna Violet', 'Aurora Jade', 'Scarlett Evangeline', 'Mila Rose', 'Stella Celeste', 'Amara Seraphina', 'Layla Skye', 'Vivienne Elise', 'Eleanor Ruby', 'Hazel Quinn', 'Aveline Ivy', 'Isla Belle', 'Nora Hazel', 'Willow Joy', 'Athena Pearl', 'Lila Maeve', 'Sophia Harper', 'Olivia Celestia', 'Isabella Claire', 'Ella Primrose', 'Grace Seraphina', 'Chloe Aurora', 'Lily Juliet', 'Ariana Eloise', 'Amelia Hope', 'Emily Florence', 'Scarlett Belle', 'Victoria Sage', 'Madeline Faye', 'Cora Everly', 'Harper Luna', 'Evelyn Aurora', 'Aria Willow', 'Luna Grace', 'Aurora Mae', 'Stella Violet', 'Vivienne Rose', 'Mila Jade', 'Layla Quinn', 'Scarlett Elise', 'Hazel Ruby', 'Aveline Seraphina', 'Isla Skye', 'Nora Celeste', 'Willow Seraphina', 'Athena Ivy', 'Lila Belle', 'Sophia Pearl', 'Olivia Maeve',
+'Penelope Grace', 'Hannah Violet', 'Sophie Camille', 'Leah Isolde', 'Evangeline Jane', 'Cecilia Mae', 'Avery Isabella', 'Natalia Everly', 'Ruby Seraphina', 'Violet Quinn', 'Zara Celestia', 'Athena Elise', 'Delilah Rose', 'Harlow Seraphina', 'Lucy Evangeline', 'Nina Belle', 'Astrid Juliet', 'Eloise Skye', 'Gemma Pearl', 'Maeve Harper', 'Juliet Ivy', 'Bianca Celeste', 'Seraphina Elara', 'Arabella Faye', 'Emery Willow', 'Elowen Maeve', 'Wren Seraphina', 'Mabel Celeste', 'Rosalie Mae', 'Serena Quinn', 'Cordelia Pearl', 'Elsie Aurora', 'Adeline Jade', 'Lila Seraphina', 'Fiona Grace', 'Tessa Celestia', 'Genevieve Ivy', 'Estelle Ruby', 'Harper Violet']
 ok = []
 cp = []
 def menu():
     os.system('clear')
     print (logo4)
-    print ('            Auto create tool')
-    print (47*'-')
-    print ('[1]auto creat')
-    print ('[2]join Facebok group')
-    print ('[3] join whatsapp group')
-    print (47*'-')
+    print ('[1] New Account Generator')
+    print (56*'-')
     sel = input('Select: ')
     if sel in['1', '01']:
         create().start()
-    elif sel in ['2', '02']:
-        os.system('xdg-open https://www.facebook.com/groups/262660289344669/?ref=share_group_link')
-        menu()
-    elif sel in ['3', '03']:
-        os.system('xdg-open https://chat.whatsapp.com/C4EokyLxEaZGBlyJ99M3pA')
-        menu()
     else:
         print ('select valid option')
         time.sleep(3)
@@ -98,27 +89,28 @@ class create:
     def start(self):
         os.system('clear')
         print (logo4)
-        print ('[1] Boys name ids')
-        print ('[2] girls name ids')
-        print (47*'-')
-        gen = input('select: ')
-        print (47*'-')
+        print ('[1] BRP Accounts')
+        print ('[2] GRP Accounts')
+        print (56*'-')
+        gen = input('Choose: ')
+        print (56*'-')
         if gen in ['1', '01']:
             self.gender.append('boy')
         elif gen in ['2', '02']:
             self.gender.append('girl')
         else:
             self.gender.append('boy')
-        print ('Example 1000, 2000, 5000, 10000')
-        lim = int(input('limit: '))
+        print ('Example: 1000, 2000, 5000, 10000')
+        print (56*'-')
+        lim = int(input('Choose Quantity: '))
         os.system('clear')
         print (logo4)
         agent = random.choice(ugen)
         headers = {
             'authority': 'm.facebook.com',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-            'sec-ch-prefers-color-scheme': 'dark',
+            'accept-language': 'en-GB,en;q=0.9,si-LK;q=0.8,si;q=0.7,en-US;q=0.6',
+            'sec-ch-prefers-color-scheme': 'light',
             'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
             'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
             'sec-ch-ua-mobile': '?1',
@@ -129,29 +121,31 @@ class create:
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': agent,
+            'user-agent': '[FBAN/FB4A;FBAV/243.0.0.2.174;FBBV/156073738;FBDM/{density=3.0,width=1080,height=1493};FBLC/en_US;FBRV/324.0.0.9.130;FBCR/Tele2 Netherlands;FBMF/motorola;FBBD/motorola;FBPN/com.facebook.katana;FBDV/XT2127-43;FBSV/10;FBOP/1;FBCA/arm64-v8a:]',
             'viewport-width': '980',
         }
         headers1 = {
             'authority': 'm.facebook.com',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-            'sec-ch-prefers-color-scheme': 'dark',
+            'accept-language': 'en-GB,en;q=0.9,si-LK;q=0.8,si;q=0.7,en-US;q=0.6',
+            'sec-ch-prefers-color-scheme': 'light',
             'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
             'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
             'sec-ch-ua-mobile': '?1',
             'sec-ch-ua-platform': '"Android"',
             'sec-ch-ua-platform-version': '"11.0.0"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
             'sec-fetch-site': 'none',
             'upgrade-insecure-requests': '1',
-            'user-agent': agent,
+            'user-agent': '[FBAN/FB4A;FBAV/243.0.0.2.174;FBBV/156073738;FBDM/{density=3.0,width=1080,height=1493};FBLC/en_US;FBRV/324.0.0.9.130;FBCR/Tele2 Netherlands;FBMF/motorola;FBBD/motorola;FBPN/com.facebook.katana;FBDV/XT2127-43;FBSV/10;FBOP/1;FBCA/arm64-v8a:]',
         }
+        print(' [•] Use airplane mode if no result. ')
+        print (56*'-')
         OO = '\033[0;97m'
         for x in range(lim):
             self.loop += 1
-            sys.stdout.write(f'\r {OO}[Creat-fb] {OO}{self.loop}/{str(lim)} OK:{len(ok)} - CP:{len(cp)}{OO} '),
+            sys.stdout.write(f'\r {OO}[ALEX-CREATING] {OO}{self.loop}/{str(lim)} | ALIVE: {len(ok)} | CHECKPOINT: {len(cp)}{OO} '),
             sys.stdout.flush()
             if 'boy' in self.gender:
                 name = random.choice(boy).split(' ')
@@ -232,7 +226,7 @@ class create:
                 cok = self.ses.cookies.get_dict()
                 if 'checkpoint' in getts.url:
                     cp.append(email+passw)
-                    print ('\r\033[1;33m[CP] '+cok['c_user']+' | '+passw+'\033[0;97m     ')
+                    print ('\r\033[1;33m [CHECKPOINT] '+cok['c_user']+' | '+passw+'\033[0;97m     ')
                 dbl = ['fb_dtsg', 'jazoest', 'flow', 'next', 'nux_source']
                 for x in BeautifulSoup(getts.text, 'html.parser').find_all('form', {'method': 'post'}):
                     if '/login/device-based/update-nonce/' in str(x):
@@ -258,11 +252,11 @@ class create:
                                 if 'checkpoint' in rex.url:
                                     cok = self.ses.cookies.get_dict()
                                     cp.append(email+passw)
-                                    print ('\r\033[1;33m[CP] '+cok['c_user']+' | '+passw+'\033[0;97m     ')                                  
+                                    print ('\r\033[1;33m [CHECKPOINT] '+cok['c_user']+' | '+passw+'\033[0;97m     ')
                                 else:
                                     coki = (";").join([ "%s=%s" % (key, value) for key, value in self.ses.cookies.get_dict().items() ])
                                     cok = self.ses.cookies.get_dict()
-                                    print ('\r\033[1;32m[OK] '+cok['c_user']+' | '+passw+' | '+coki+'\033[0;97m     ')                                   
+                                    print ('\r\033[1;32m [ALEX-ALIVE] '+cok['c_user']+' | '+passw+' | '+coki+'\033[0;97m     ')
                                     ok.append(email+passw)
             except requests.exceptions.ConnectionError:
                 time.sleep(1)
@@ -270,9 +264,9 @@ class create:
             except Exception as e:
                 pass
         print ('process has been completed')
-        print (47*'-')
-        print ('\033[1;32mTotal ids > Ok/' +str(len(ok)) + ' CP/' + str(len(cp)))
-        print (47*'-')
+        print (56*'-')
+        print ('\033[1;32mTotal ids > ALIVE/' +str(len(ok)) + ' CHECKPOINT/' + str(len(cp)))
+        print (56*'-')
         input('back')
         menu()
 menu()
